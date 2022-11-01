3e4b2fca834f937a8d6127dec237e2fe6b75bb21
Tristina.Nguyen <tristina1788@gmail.com>
2022-11-01 12:49:13 +0700

    update ci/cd

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,32 @@ module.exports = defineConfig({
     "defaultCommandTimeout": 60000,
     "pageLoadTimeout": 90000,
     "video": false,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+        configFile: "config/reporterOpts.json",
+    },
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
         // use mochawesome reporter as usually
-        "reporter": "junit",
-        "reporterOptions": {
-            "mochaFile": "tests/test-output.xml",
-            "toConsole": true,
-            "attachments": true
-        },
+        // "reporter": "junit",
+        // "reporterOptions": {
+        //     "mochaFile": "tests/test-output.xml",
+        //     "toConsole": true,
+        //     "attachments": true
+        // },
+        fixturesFolder: "cypress/fixtures",
+        downloadsFolder: "cypress/downloads",
+        screenshotsFolder: "reports/screenshots",
+        videosFolder: "reports/videos",
+        viewportWidth: 1024,
+        viewportHeight: 800,
+        videoCompression: 38,
+        numTestsKeepInMemory: 1,
+        supportFile: "support/e2e.js",
+        specPattern: "e2e/**/*.cy.{js,jsx,ts,tsx}",
+        video: true,
     },
     "watchForFileChanges": false,
     "chromeWebSecurity": false,
